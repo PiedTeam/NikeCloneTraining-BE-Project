431df14e2392cb7409a4c09be5a93527ba486ed6
BLonggg608 <tranhabaolong608@gmail.com>
2024-04-10 20:55:30 +0700

    feat: create paramSchema for common validation field

---
+++ src/modules/user/user.messages.ts
@@ -0,0 +1,37 @@
+export const USER_MESSAGES = {
+    // username
+    USERNAME_IS_REQUIRED: 'Username is required',
+    USERNAME_MUST_BE_STRING: 'Username must be a string',
+    USERNAME_LENGTH_MUST_BE_FROM_1_TO_50:
+        'Username must be between 1 and 50 characters',
+
+    // email
+    EMAIL_IS_REQUIRED: 'Email is required',
+    EMAIL_IS_INVALID: 'Email is invalid',
+
+    // phone number
+    PHONE_NUMBER_IS_REQUIRED: 'Phone number is required',
+    PHONE_NUMBER_MUST_BE_STRING: 'Phone number must be a string',
+    PHONE_NUMBER_IS_INVALID: 'Phone number is invalid',
+
+    // password
+    PASSWORD_IS_REQUIRED: 'Password is required',
+    PASSWORD_MUST_BE_STRING: 'Password must be a string',
+    PASSWORD_MUST_BE_STRONG: 'Password must be strong',
+
+    // confirm password
+    CONFIRM_PASSWORD_MUST_MATCH_PASSWORD:
+        'Confirm password must match password',
+
+    // first name
+    FIRST_NAME_IS_REQUIRED: 'First name is required',
+    FIRST_NAME_MUST_BE_STRING: 'First name must be a string',
+    FIRST_NAME_LENGTH_MUST_BE_FROM_1_TO_50:
+        'First name must be between 1 and 50 characters',
+
+    // last name
+    LAST_NAME_IS_REQUIRED: 'Last name is required',
+    LAST_NAME_MUST_BE_STRING: 'Last name must be a string',
+    LAST_NAME_LENGTH_MUST_BE_FROM_1_TO_50:
+        'Last name must be between 1 and 50 characters'
+}
---
+++ src/modules/user/user.middlewares.ts
@@ -0,0 +1,127 @@
+import { ParamSchema } from 'express-validator'
+import { USER_MESSAGES } from './user.messages'
+import { error } from 'console'
+
+const usernameSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.USERNAME_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.USERNAME_MUST_BE_STRING
+    },
+    trim: true,
+    isLength: {
+        options: {
+            min: 1,
+            max: 50
+        },
+        errorMessage: USER_MESSAGES.USERNAME_LENGTH_MUST_BE_FROM_1_TO_50
+    }
+}
+
+const emailSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.EMAIL_IS_REQUIRED
+    },
+    isEmail: {
+        errorMessage: USER_MESSAGES.EMAIL_IS_INVALID
+    },
+    trim: true
+}
+
+const phone_numberSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.PHONE_NUMBER_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.PHONE_NUMBER_MUST_BE_STRING
+    },
+    isMobilePhone: {
+        options: ['vi-VN'],
+        errorMessage: USER_MESSAGES.PHONE_NUMBER_IS_INVALID
+    }
+}
+
+const passwordSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.PASSWORD_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.PASSWORD_MUST_BE_STRING
+    },
+    trim: true,
+    isStrongPassword: {
+        options: {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        },
+        errorMessage: USER_MESSAGES.PASSWORD_MUST_BE_STRONG
+    }
+}
+
+const confirmPasswordSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.PASSWORD_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.PASSWORD_MUST_BE_STRING
+    },
+    trim: true,
+    isStrongPassword: {
+        options: {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        },
+        errorMessage: USER_MESSAGES.PASSWORD_MUST_BE_STRONG
+    },
+    custom: {
+        options: (value, { req }) => {
+            if (!value === req.body.password) {
+                throw new Error(
+                    USER_MESSAGES.CONFIRM_PASSWORD_MUST_MATCH_PASSWORD
+                )
+            }
+            return true
+        }
+    }
+}
+
+const firstnameSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.FIRST_NAME_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.FIRST_NAME_MUST_BE_STRING
+    },
+    trim: true,
+    isLength: {
+        options: {
+            min: 1,
+            max: 50
+        },
+        errorMessage: USER_MESSAGES.FIRST_NAME_LENGTH_MUST_BE_FROM_1_TO_50
+    }
+}
+
+const lastnameSchema: ParamSchema = {
+    notEmpty: {
+        errorMessage: USER_MESSAGES.LAST_NAME_IS_REQUIRED
+    },
+    isString: {
+        errorMessage: USER_MESSAGES.LAST_NAME_MUST_BE_STRING
+    },
+    trim: true,
+    isLength: {
+        options: {
+            min: 1,
+            max: 50
+        },
+        errorMessage: USER_MESSAGES.LAST_NAME_LENGTH_MUST_BE_FROM_1_TO_50
+    }
+}
