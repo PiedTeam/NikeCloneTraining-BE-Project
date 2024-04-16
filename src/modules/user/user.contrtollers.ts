2d9d58930f9101089ca2382204eed23b77a79465
BLonggg608 <tranhabaolong608@gmail.com>
2024-04-16 19:55:24 +0700

    feat: login with username, email, phone number

--- src/modules/user/user.contrtollers.ts
+++ src/modules/user/user.contrtollers.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import usersService from './user.services'
-import { RegisterReqBody } from './user.requests'
+import { LoginRequestBody, RegisterReqBody } from './user.requests'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { USER_MESSAGES } from './user.messages'
+import User from './user.schema'
+import { ObjectId } from 'mongodb'
 
 export const registerController = async (
     req: Request<ParamsDictionary, any, RegisterReqBody>,
@@ -12,6 +14,19 @@ export const registerController = async (
     const result = await usersService.register(req.body)
     return res.json({
         message: USER_MESSAGES.REGISTER_SUCCESS,
+        data: result
+    })
+}
+
+export const loginController = async (
+    req: Request<ParamsDictionary, any, LoginRequestBody>,
+    res: Response,
+    next: NextFunction
+) => {
+    const user_id = (req.user as User)._id as ObjectId
+    const result = await usersService.login(user_id.toString() as string)
+    res.json({
+        message: USER_MESSAGES.LOGIN_SUCCESS,
         data: result
     })
 }
