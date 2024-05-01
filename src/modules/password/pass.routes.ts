4c730bbb4d019077c5dc39af0eef59b4b077518f
Heromine99 <doanhluong59@gmail.com>
2024-05-01 23:27:35 +0700

    feat:entryPasswordAfterRegisterByGG/FBAPI

---
+++ src/modules/password/pass.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { wrapAsync } from '~/utils/handler'
+import { registerPassword } from './pass.controllers'
+import { registerPasswordValidator } from './pass.middleware'
+
+const passwordRouter = Router()
+passwordRouter.post(
+    '/updatePass',
+    registerPasswordValidator,
+    wrapAsync(registerPassword)
+)
+
+export default passwordRouter
