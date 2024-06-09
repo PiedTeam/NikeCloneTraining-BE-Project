453c1ccd2154c7ca8f967b96b6bea20a2c72ebc7
minatisleeping <minatt2002@gmail.com>
2024-06-09 14:32:55 +0700

    Fix: update me allow user to update password..

--- src/modules/user/user.services.ts
+++ src/modules/user/user.services.ts
@@ -290,6 +290,8 @@ class UsersService {
         user_id: string
         payload: UpdateMeReqBody
     }) {
+        if (payload.password) payload.password = hashPassword(payload.password)
+
         const user = await databaseService.users.findOneAndUpdate(
             { _id: new ObjectId(user_id) },
             [
@@ -309,6 +311,7 @@ class UsersService {
                 }
             }
         )
+
         return user
     }
 
