4027b4246478ca9a663b52fa475d160c04960001
lcaohoanq <hoangclw@gmail.com>
2024-06-19 16:10:22 +0700

    refactor: add function generateOtp > change interface to type > remove redundant check isExist user

--- src/modules/otp/otp.middlewares.ts
+++ src/modules/otp/otp.middlewares.ts
@@ -1,29 +1,12 @@
 import { checkSchema } from 'express-validator'
-import { USER_MESSAGES } from '../user/user.messages'
 import { validate } from '~/utils/validation'
-import usersService from '../user/user.services'
-import { phone_numberSchema, emailSchema } from '../user/user.middlewares'
-import { encrypt } from '~/utils/crypto'
+import { emailSchema, phone_numberSchema } from '../user/user.middlewares'
 
 export const phoneNumberValidator = validate(
     checkSchema(
         {
             phone_number: {
-                ...phone_numberSchema,
-                custom: {
-                    options: async (value) => {
-                        const isExist =
-                            await usersService.checkPhoneNumberExist(
-                                encrypt(value)
-                            )
-                        if (!isExist) {
-                            throw new Error(
-                                USER_MESSAGES.PHONE_NUMBER_IS_NOT_REGISTERED
-                            )
-                        }
-                        return true
-                    }
-                }
+                ...phone_numberSchema
             }
         },
         ['body']
@@ -34,20 +17,7 @@ export const emailValidator = validate(
     checkSchema(
         {
             email: {
-                ...emailSchema,
-                custom: {
-                    options: async (value) => {
-                        const isExist = await usersService.checkEmailExist(
-                            encrypt(value)
-                        )
-                        if (!isExist) {
-                            throw new Error(
-                                USER_MESSAGES.EMAIL_IS_NOT_REGISTERED
-                            )
-                        }
-                        return true
-                    }
-                }
+                ...emailSchema
             }
         },
         ['body']
